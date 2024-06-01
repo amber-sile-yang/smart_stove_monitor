2667ad7d0e7ad63f6e054b1aa9d8dbccd6e125bc
Amber Yang <amberyang@Ambers-MacBook-Pro.local>
2024-06-01 19:06:32 -0400

    implemented interface with motion sensor & LED, hw tested to be ok

--- Src/main.c
+++ Src/main.c
@@ -15,29 +15,96 @@
  ******************************************************************************
  */
 
-
 #include "stm32f4xx.h"
 
 // Function prototypes
 void GPIO_Init(void);
-
+void timer_delay_ms(uint32_t delay_ms);
 
 int main(void) {
+
     //SystemInit();
+
     GPIO_Init();
+
+    while(1){
+
+    	// turn on the LED if the motion is detected
+    	if(GPIOA -> IDR & GPIO_IDR_IDR_0){
+    		GPIOA -> ODR |= GPIO_ODR_ODR_1;
+    	}
+    	else{
+    		GPIOA -> ODR &= ~GPIO_ODR_ODR_1;
+    	}
+
+    	// 1s delay
+    	//timer_delay_ms(1000);
+    }
 
 }
 
 // SystemInit
 // ...
 
-// GPIO_Init
+// GPIO initialization
 void GPIO_Init(void) {
 
-	// enable clk for GPIOA
+	// enable clk for GPIOA: bit 0 of AHB1ENR is set
 	RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-	// set PA0 as i/p for motion sensor
+	// set PA0 as i/p for motion sensor: bits 1:0 of MODER set to 00
+	GPIOA -> MODER &= ~(GPIO_MODER_MODER0);
+
+	// set PA1 as o/p for LED: bits 3:2 set to 01
+	GPIOA -> MODER |= GPIO_MODER_MODER1_0;
 
 
 }
+
+// ms delay using timer
+void timer_delay_ms(uint32_t delay_ms){
+
+	//enable tim2 clk
+	RCC -> APB1ENR |= RCC_APB1ENR_TIM2EN;
+
+	// set 1kHz (from 16MHz src) to achieve 1/1kHz = 1ms for each count
+	// CK_CNT = fCK_PSC / (PSC[15:0] + 1)
+	// 1kHz = 16MHz / (prescale_value + 1 )
+	TIM2 -> PSC = (16000000/1000)-1;
+
+	// load the desired delay
+	TIM2 -> ARR = delay_ms;
+
+	// start counting from 0
+	TIM2 -> CNT = 0;
+
+	// enable the counter: bit 0 of CR1 is set
+	TIM2 -> CR1 |= TIM_CR1_CEN;
+
+	// keep looping while Update Interrupt Flag (UIF) is not set in Status Register (SR)
+	while(!(TIM2 -> SR & TIM_SR_UIF)){}
+
+	// clc UIF after it is set
+	TIM2 -> SR &= ~(TIM_SR_UIF);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
